f804ad31021f75315eb478822f42441bca64efa6
prithviRajo0015314 <pkengineeringoffice@gmail.com>
2024-08-18 09:00:52 +0530

    check js
    none

---
+++ media.js
@@ -0,0 +1,10 @@
+let text = document.querySelector('h1');
+let button = document.getElementById('button');
+
+
+function fun()
+{
+    text.style.color="red"; 
+    text.style.fontFamily="agency fb"; 
+
+}
